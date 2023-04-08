b6c65089280af50cb93c4893cd084e326a6ce41e
bab0n <babonoffishial@mail.ru>
2023-04-08 05:06:45 +0300

    Добавил создание админа по умолчанию

--- create_db.py
+++ create_db.py
@@ -3,3 +3,8 @@ from project import db, create_app, models
 if __name__ == '__main__':
     with create_app().app_context():
         db.create_all()
+
+        # Создание стандартного админа для регистрации новых пользователей
+        new_user = models.User(login='admin', password='admin123', status='#admin')
+        db.session.add(new_user)
+        db.session.commit()
